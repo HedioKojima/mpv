29cf725bb348bc92f372a75c6f2fe38d1e3da0d0
HedioKojima <46861125+HedioKojima@users.noreply.github.com>
2026-08-12 19:05:46 +0800

    Remove build_subrandr function from build-common.sh

--- ci/build-common.sh
+++ ci/build-common.sh
@@ -1,13 +1,6 @@
 common_args="--werror \
--Dlibmpv=true \
--Dtests=true \
+-Dlibmpv=false \
+-Dtests=false \
 "
 
 export CFLAGS="$CFLAGS -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3"
-
-build_subrandr() {
-    git clone --depth=1 https://github.com/afishhh/subrandr.git
-    pushd subrandr
-    cargo xtask install --prefix "$@"
-    popd
-}
